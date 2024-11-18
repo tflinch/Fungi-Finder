1721c1fd2258f602f5a7cd950cf441afd78f6515
tflinch <tflinch16@gmail.com>
2024-11-18 14:40:30 -0500

    feat: updated reszizing

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ navToggle.addEventListener('click', () => {
     navToggle.setAttribute('aria-expanded', 'false');
   }
 });
+
+const resizeObserver = new ResizeObserver(() => {
+  document.body.classList.add('resizing');
+
+  requestAnimationFrame(() => {
+    document.body.classList.remove('resizing');
+  });
+});
+
+resizeObserver.observe(document.body);
